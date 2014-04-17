0a17a092ec383816ec7dc8d14949b1aad75193dc
Dan Calacci <dcalacci@achtung05.ccs.neu.edu>
2014-04-17 17:34:13 -0400

    small changes to generating distance

--- src/generate_distance_map.py
+++ src/generate_distance_map.py
@@ -5,28 +5,28 @@ import shapely
 import fiona
 import os
 
-# dataframe with trains info
-trains_df = pd.read_csv('../notebooks/trains_df.p')
-
-# has grid location info
-grid_loc_df = pd.read_csv('../notebooks/grid_loc_df.csv')
-
 
 def create_nearest_distance_df():
+    # dataframe with trains info
+    trains_df = pd.read_csv('../notebooks/trains_df.p')
+
+    # has grid location info
+    grid_loc_df = pd.read_csv('../notebooks/grid_loc_df.csv')
+
     closest_dist = []
     nearest_station = []
 
     for n, row in grid_loc_df.iterrows():
-        if n % 10000 == 0:
+        if n % 1000 == 0:
             print "processed ", float(n)/len(grid_loc_df)*100, "% of elements."
-        res = utils.get_nearest_station((row.x, row.y), trains_df)
+        res = utils.get_nearest_station((float(row.x), float(row.y)), trains_df)
 
         nearest_station.append(res[0])
         closest_dist.append(res[1])
 
     grid_loc_df['closest_distance'] = closest_dist
     grid_loc_df['nearest_station'] = nearest_station
-    grid_loc_df.to_csv('grid_loc_df_with_dist.csv')
+    grid_loc_df.to_pickle('grid_loc_df_with_dist.p')
 
 
 def create_shapefile(shapefile, grid_df):
